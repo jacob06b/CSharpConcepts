5a8dfdc6656a75a7aa8980537befe3765f71b57b
Jacob Bramwell <jacob.bramwell@icloud.com>
2022-09-22 15:06:26 +0100

    Update SelectionExercises.cs

--- CSharpConcepts.Classes/Exercises/SelectionExercises.cs
+++ CSharpConcepts.Classes/Exercises/SelectionExercises.cs
@@ -8,54 +8,54 @@ namespace CSharpConcepts.Classes.Exercises
 {
     public static class SelectionExercises
     {
- 
+
 
         // Q1: FizzBuzz
         // Return Fizz if divisible by 3. Buzz if divisible by 5. FizzBuzz if divisible by both.
         // If divisible by neither return the number itself (as a string)
-//            public static string FizzBuzz(int num)
-//        {
-//            bool fizzcheck = false;
-//            bool buzzcheck = false;
-//            int div3 = num % 3;
-//            int div5 = num % 5;
-//            if (div3 == 0)
-//            {
-//                bool fizzcheck = true;
-//            }
-//            else
-//            {
-//                bool fizzcheck = false;
-//            }
-//            if (div5 == 0)
-//            {
-//                bool buzzcheck = true;
-//            }
-//            else
-//            {
-//                bool buzzcheck = false;
-//            }
-//            if (fizzcheck == true)
-//            {
-//                if (buzzcheck == true)
-//                {
-//                    Console.WriteLine("FizzBuzz");
-//                }
-//                else
-//                {
-//                    Console.WriteLine("Fizz");
-//                }
-//            }
-//            else if (buzzcheck == true)
-//            {
-//                Console.WriteLine("Buzz");
-//            }
-//            else
-//            {
-//                Console.WriteLine(num);
-//            }
-//        }
-//
+        //            public static string FizzBuzz(int num)
+        //        {
+        //            bool fizzcheck = false;
+        //            bool buzzcheck = false;
+        //            int div3 = num % 3;
+        //            int div5 = num % 5;
+        //            if (div3 == 0)
+        //            {
+        //                bool fizzcheck = true;
+        //            }
+        //            else
+        //            {
+        //                bool fizzcheck = false;
+        //            }
+        //            if (div5 == 0)
+        //            {
+        //                bool buzzcheck = true;
+        //            }
+        //            else
+        //            {
+        //                bool buzzcheck = false;
+        //            }
+        //            if (fizzcheck == true)
+        //            {
+        //                if (buzzcheck == true)
+        //                {
+        //                    Console.WriteLine("FizzBuzz");
+        //                }
+        //                else
+        //                {
+        //                    Console.WriteLine("Fizz");
+        //                }
+        //            }
+        //            else if (buzzcheck == true)
+        //            {
+        //                Console.WriteLine("Buzz");
+        //            }
+        //            else
+        //            {
+        //                Console.WriteLine(num);
+        //            }
+        //        }
+        //
         // Q2: Vowel or Consonant
         // Using a switch statement write a function which returns true if the input is a vowel and
         // false if it is a consonant
@@ -76,7 +76,7 @@ namespace CSharpConcepts.Classes.Exercises
                 Console.WriteLine("False");
                 return false;
             }
-            
+
         }
 
         // Q3: DiceRoller
@@ -113,16 +113,31 @@ namespace CSharpConcepts.Classes.Exercises
                 Console.WriteLine("Sorry, you lose!");
             }
             return die1;
-        }  
+        }
         // Q4: Angles
         // Write a function to return whether the angle input is acute, right angle, obtuse, straight or reflex
         // Use an enum for the different angle types
-
+        public enum AngleSizes
+        {
+            Acute,
+            RightAngle,
+            Straight,
+            Obtuse,
+            Reflex
+        }
+        public static string Angles(int angle);
+        {
+            int angleNum;
+            switch (angle)
+            {
+            }
+        }
         // Q5: Rock, Paper Scissors
         // Write a procedure which takes the user's choice as input and randomly generate the computer's choice
         // It should write the computer's choice to the console and write whether the user lost, won, or drew.
         // Handle the user input in the calling code in Program.cs
-
+        public static string rpc(string choice)
+        
         // Q6: Triangle
         // Write a function that takes in three side lengths.
         // Determine whether a triangle with those sides is equilateral, isosceles, scalene or impossible
